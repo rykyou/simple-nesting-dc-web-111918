493038bdc180eda681692fc35e30720359464058
Robin Kim <rkim1214@gmail.com>
2018-10-24 21:02:46 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -59,12 +59,6 @@ def dennis_ritchies_language
   programmer_hash[:dennis_ritchie][:languages].join
 end
 
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
-
 def adding_matz
 	programmer_hash = 
  		{
